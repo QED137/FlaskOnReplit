05a9b1799e3b3470a988e00afa54bffa20ffc445
Janmajay Kumar <106250110+QED137@users.noreply.github.com>
2024-05-09 10:57:51 +0000

    bootstrap has been added

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 from flask import Flask, render_template, request, redirect
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 
+JOBS =[
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Tübingen, Germany',
+    'Sallary' : '€15.000'
+    
+  },
+  {
+    'id': 2,
+    'title': 'Data Engineer',
+    'location': 'Münich, Germany',
+    'Sallary' : '€34.000'
+
+  },
+  {
+    'id': 3,
+    'title': 'Machine Learning Expert',
+    'location': 'Berlin, Germany',
+    'Sallary' : '€115.000'
+
+  }
+]
+    
 @app.route("/")
 
 def hello_world():
+  
   return render_template('index.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
-
-  
