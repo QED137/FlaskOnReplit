a9d7bfbeef6f6b5f74c470ef932f697ffc23e939
Janmajay Kumar <106250110+QED137@users.noreply.github.com>
2025-05-27 09:59:55 +0000

    google database added

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 import logging
 from sqlalchemy.sql import func
-from database import load_data_from_db, load_job_fromDB #push_data_to_db
+from database import load_data_from_db #push_data_to_db
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -19,45 +19,46 @@ def hello_world():
   return render_template('index.html', jobs=JOBS)
   
 
-'''This function retrieves a list of jobs from the database and returns them as JSON data. The data is read-only and cannot be modified. This function serves as an API endpoint for accessing job listings.'''
+# '''This function retrieves a list of jobs from the database and returns them as JSON data. The data is read-only and cannot be modified. This function serves as an API endpoint for accessing job listings.'''
 @app.route("/api/jobs")
 
 def list_jobs():
   JOBS=load_data_from_db()
   return jsonify(JOBS)
+    
 
-@app.route("/job/<id>")
+# @app.route("/job/<id>")
 
-def show_job(id):
-  job = load_job_fromDB(id)
-  if not job:
-    return "Not Found", 404
-  return  render_template('jobpage.html', job=job)
-  #return jsonify(job)
-@app.route("/job/<id>/apply", methods=['POST'])
-def apply_to_job(id):
-    data = request.form.to_dict()
-    logging.debug(f"Received application data: {data}")
-    job = load_job_fromDB(id)
-    if not job:
-        return "Job Not Found", 404
-    try:
-        push_data_to_db(id, data)
-        logging.info(f"Application data for job {id} inserted successfully")
-        return render_template('applicationSubmitted.html', applications=data, job=job)
-    except Exception as e:
-        logging.error(f"Error applying to job: {e}")
-        return "An error occurred while processing your application", 500
+# def show_job(id):
+#   job = load_job_fromDB(id)
+#   if not job:
+#     return "Not Found", 404
+#   return  render_template('jobpage.html', job=job)
+#   #return jsonify(job)
+# @app.route("/job/<id>/apply", methods=['POST'])
+# def apply_to_job(id):
+#     data = request.form.to_dict()
+#     logging.debug(f"Received application data: {data}")
+#     job = load_job_fromDB(id)
+#     if not job:
+#         return "Job Not Found", 404
+#     try:
+#         push_data_to_db(id, data)
+#         logging.info(f"Application data for job {id} inserted successfully")
+#         return render_template('applicationSubmitted.html', applications=data, job=job)
+#     except Exception as e:
+#         logging.error(f"Error applying to job: {e}")
+#         return "An error occurred while processing your application", 500
 
   
-'''if request.method == 'POST':
-        try:
-            data = request.form
-            return jsonify(data)
-        except Exception as e:
-            return jsonify({'error': str(e)})
-    else:
-        return jsonify({'error': 'Only POST requests are allowed for this route'})'''
+# '''if request.method == 'POST':
+#         try:
+#             data = request.form
+#             return jsonify(data)
+#         except Exception as e:
+#             return jsonify({'error': str(e)})
+#     else:
+#         return jsonify({'error': 'Only POST requests are allowed for this route'})'''
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
--- database.py
+++ database.py
@@ -1,17 +1,91 @@
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, DateTime, text
-from sqlalchemy.engine import result
-import os
-import logging
-from sqlalchemy.exc import SQLAlchemyError
+# '''from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, DateTime, text
+# from sqlalchemy.engine import result
+# import os
+# import logging
+# from sqlalchemy.exc import SQLAlchemyError
 
-engine = create_engine("mysql+pymysql://myfirstDB_goprettywe:b5f69eae773cb5af8088dcefc48e1e17ce71e8f0@8zg.h.filess.io/myfirstDB_goprettywe?charset=utf8mb4")
-#print(type(engine))
-#how to get data back from mysql
-# Establish a connection to the database and execute a query
+# engine = create_engine("mysql+pymysql://myfirstDB_goprettywe:b5f69eae773cb5af8088dcefc48e1e17ce71e8f0@8zg.h.filess.io/myfirstDB_goprettywe?charset=utf8mb4")
+# #print(type(engine))
+# #how to get data back from mysql
+# # Establish a connection to the database and execute a query
+# def load_data_from_db():
+#   with engine.connect() as conn:
+#     # Execute a SQL query to select all rows from the 'posts' table
+#     result = conn.execute(text("SELECT * FROM posts"))
+
+#     # Fetch column names from the query result
+#     columns = result.keys()  
+
+#     # Convert the query result into a list of dictionaries, with column names as keys
+#     result_dicts = [dict(zip(columns, row)) for row in result.fetchall()]
+
+#     # Print the list of dictionaries representing the query result
+#     return result_dicts
+# def load_job_fromDB(id):
+#     with engine.connect() as conn:
+#         result = conn.execute(text("SELECT * FROM posts WHERE id = :val"), {"val": id})
+#         rows = result.fetchall()
+#         if len(rows) == 0:
+#             return None
+#         else:
+#             return rows[0]
+
+# print(load_job_fromDB(1))
+# #the following code is for retriveing data from the webpage and psuhing it into the database
+
+# '''
+# import os
+# import psycopg2
+# from flask import Flask, render_template
+# def get_db_connection():
+#     return psycopg2.connect(
+#         host='dpg-cq95unrv2p9s73cdvrv0-a',
+#         database='myfirstdb_gaoa',
+#         user=os.getenv('DATABASE_USER', 'jay'),
+#         password=os.getenv('DATABASE_PASSWORD', 'oDv9xYgw1cywbb6WASW9wM9BHiUoeILs')
+#     )
+
+# try:
+#     conn = get_db_connection()
+#     print("Connection successful")
+#     conn.close()
+# except Exception as e:
+#     print(f"Error connecting to the database: {e}")
+
+from functools import partialmethod
+import sqlalchemy
+from sqlalchemy import create_engine, text
+import os
+#print(sqlalchemy.__version__)
+
+
+my_secret = os.environ['GOOGLE_MYSQL_DB']
+
+
+engine = create_engine(my_secret)
+
+# Test the connection by trying to connect
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+        # You can also execute a test query
+        result = connection.execute(text("SELECT * from jobs"))
+        #print(result.all()[0])
+        result_dicts = []
+        for row in result.all():
+            result_dicts.append(dict(zip(result.keys(), row)))
+        print(result_dicts)    
+        #print(result.all())  # Should print (1,)
+        #print(type(result))
+        #print(type(result.all()))
+        #print(result.all())
+except Exception as e:
+    print(f"Connection failed: {e}")
+
 def load_data_from_db():
   with engine.connect() as conn:
     # Execute a SQL query to select all rows from the 'posts' table
-    result = conn.execute(text("SELECT * FROM posts"))
+    result = conn.execute(text("SELECT * FROM jobs"))
 
     # Fetch column names from the query result
     columns = result.keys()  
@@ -21,15 +95,5 @@ def load_data_from_db():
 
     # Print the list of dictionaries representing the query result
     return result_dicts
-def load_job_fromDB(id):
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM posts WHERE id = :val"), {"val": id})
-        rows = result.fetchall()
-        if len(rows) == 0:
-            return None
-        else:
-            return rows[0]
-
-print(load_job_fromDB(1))
-#the following code is for retriveing data from the webpage and psuhing it into the database
-
+result1 = load_data_from_db()
+print(result1[1])
